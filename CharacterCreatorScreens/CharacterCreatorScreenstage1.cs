c3842ac474d0077c8def9192cd168004467ab107
Kacper Tessmer <kacper.tess@protonmail.com>
2025-01-27 21:17:41 +0100

    Final Versionx2

--- CharacterCreatorScreens/CharacterCreatorScreenstage1.cs
+++ CharacterCreatorScreens/CharacterCreatorScreenstage1.cs
@@ -7,7 +7,7 @@ class CharacterCreatorScreen1 : ScreenObject
     private ScreenSurface _mainSurface;
 
     private int Slider = 5;
-    int thiefbility = 5;
+    int race = 5;
 
     List<Color> selectedColor = new List<Color> {new Color(54, 29, 17),  new Color(78, 42, 24),  new Color(102, 51, 26), new Color(128, 64, 32), new Color(153, 76, 38), new Color(179, 102, 51), new Color(204, 153, 102), new Color(230, 184, 138), new Color(240, 210, 170), new Color(250, 224, 196)};
     List<String> CarnationName = new List<String> {"Ciemny braz","Braz","Jasny braz","Ciemny karmel","Karmel","Jasny karmel","Ciemny bez","Bez","Jasny bez","Bardzo jasny bez"};
@@ -34,8 +34,8 @@ class CharacterCreatorScreen1 : ScreenObject
                 {
                     _mainSurface.Fill(new Rectangle(20 + (Slider * 4), 20, 4, 2), Color.White, Color.Gray, 0, Mirror.None);
                     Slider++;
-                    thiefbility++;
-                    DrawingTools.DrawAvatar(_mainSurface, selectedColor[thiefbility]);
+                    race++;
+                    DrawingTools.DrawAvatar(_mainSurface, selectedColor[race]);
                 }
             }
 
@@ -45,19 +45,19 @@ class CharacterCreatorScreen1 : ScreenObject
                 {
                     _mainSurface.Fill(new Rectangle(16 + (Slider * 4), 20, 4, 2), Color.White, Color.White, 0, Mirror.None);
                     Slider--;
-                    thiefbility--;
-                    DrawingTools.DrawAvatar(_mainSurface, selectedColor[thiefbility]);
+                    race--;
+                    DrawingTools.DrawAvatar(_mainSurface, selectedColor[race]);
                 }
             }
 
             if (keyboard.IsKeyPressed(SadConsole.Input.Keys.Enter))
             {
 
-                _mainSurface.Print(30, 22, $"Zatwierdzono! Karnacja to: {CarnationName[thiefbility]}");
+                _mainSurface.Print(30, 22, $"Zatwierdzono! Karnacja to: {CarnationName[race]}");
 
                 PlayerStats player = new PlayerStats
                 {
-                Carnation = CarnationName[thiefbility],
+                Carnation = CarnationName[race],
                 };
 
                 if (player.Carnation != null && player.Carnation != "")
@@ -68,7 +68,7 @@ class CharacterCreatorScreen1 : ScreenObject
                 {
                     DrawingTools.DrawBlankAvatarPlace(_mainSurface);
                 }
-                PlayerStats.UpdateStat("./Data/playerstats.json", "CarnationRGBcode", selectedColor[thiefbility] );
+                PlayerStats.UpdateStat("./Data/playerstats.json", "CarnationRGBcode", selectedColor[race] );
 
                 SadConsole.Game.Instance.Screen = new CharacterCreatorScreen2();
 
